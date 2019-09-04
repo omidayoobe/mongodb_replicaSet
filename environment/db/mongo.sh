b53025c43e40bc01ba7b916f35f38dfa332f3de3
Omid Ayoobe <OAyoobe@spartaglobal.com>
2019-09-04 09:46:56 +0100

    added static ips on the host file and the mongo.sh

--- environment/db/mongo.sh
+++ environment/db/mongo.sh
@@ -2,6 +2,6 @@ mongo
 echo 'rs.initiate({
       _id: "rs0",
       members: [
-         { _id: 0, host: "Mongo1:27017" },
-         { _id: 1, host: "Mongo2:27017" },
-         { _id: 2, host: "Mongo3:27017", arbiterOnly:false }]});' | mongo
+         { _id: 0, host: "10.0.15.150" },
+         { _id: 1, host: "10.0.15.151" },
+         { _id: 2, host: "10.0.15.152", arbiterOnly:false }]});' | mongo
